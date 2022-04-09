3ce3ebb3ef32d79db7a940b4a69f81033bf7584f
Ítalo Maciel <italomacielp>
2022-04-09 14:42:49 -0300

    Adicionando o método de buscar todos

--- src/main/java/br/com/springboot/curso_springboot/controllers/GreetingsController.java
+++ src/main/java/br/com/springboot/curso_springboot/controllers/GreetingsController.java
@@ -1,10 +1,15 @@
 package br.com.springboot.curso_springboot.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -39,4 +44,16 @@ public class GreetingsController {
     	
         return "Hello " + name + "!";
     }
+    
+    @GetMapping(value = "listall") /*1º método da API - Buscar todos*/
+    @ResponseBody /*Retorna os dados par ao corpo da resposta*/
+    public ResponseEntity<List<Users>> listaUsuario(){
+    	
+    	List<Users> users = userRepository.findAll(); /*executa a consulta no banco de dados*/
+    	
+    	return new ResponseEntity<List<Users>>(users, HttpStatus.OK); /*Retorna a lista em JSON, o status
+    	serve para indicar que deu tudo certo.*/
+    	
+    }
+    
 }
